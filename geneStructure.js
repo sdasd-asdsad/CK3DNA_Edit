58f9f62c957253816403317dccf5851c7095596b
sda <nanjin96@snu.ac.kr>
2020-09-11 18:24:20 +0900

    new feature : make gene homogeneous

---
+++ geneStructure.js
@@ -0,0 +1,1249 @@
+gene_map_raw = `{
+	"hair_color": {
+		"index": 0
+	},
+	"skin_color": {
+		"index": 1
+	},
+	"eye_color": {
+		"index": 2
+	},
+	"gene_chin_forward": {
+		"index": 3,
+		"chin_forward_neg": {
+			"index": 0
+		},
+		"chin_forward_pos": {
+			"index": 1
+		}
+	},
+	"gene_chin_height": {
+		"index": 4,
+		"chin_height_neg": {
+			"index": 0
+		},
+		"chin_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_chin_width": {
+		"index": 5,
+		"chin_width_neg": {
+			"index": 0
+		},
+		"chin_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_eye_angle": {
+		"index": 6,
+		"eye_angle_neg": {
+			"index": 0
+		},
+		"eye_angle_pos": {
+			"index": 1
+		}
+	},
+	"gene_eye_depth": {
+		"index": 7,
+		"eye_depth_neg": {
+			"index": 0
+		},
+		"eye_depth_pos": {
+			"index": 1
+		}
+	},
+	"gene_eye_height": {
+		"index": 8,
+		"eye_height_neg": {
+			"index": 0
+		},
+		"eye_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_eye_distance": {
+		"index": 9,
+		"eye_distance_neg": {
+			"index": 0
+		},
+		"eye_distance_pos": {
+			"index": 1
+		}
+	},
+	"gene_eye_shut": {
+		"index": 10,
+		"eye_shut_neg": {
+			"index": 0
+		},
+		"eye_shut_pos": {
+			"index": 1
+		}
+	},
+	"gene_forehead_angle": {
+		"index": 11,
+		"forehead_angle_neg": {
+			"index": 0
+		},
+		"forehead_angle_pos": {
+			"index": 1
+		}
+	},
+	"gene_forehead_brow_height": {
+		"index": 12,
+		"forehead_brow_height_neg": {
+			"index": 0
+		},
+		"forehead_brow_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_forehead_roundness": {
+		"index": 13,
+		"forehead_roundness_neg": {
+			"index": 0
+		},
+		"forehead_roundness_pos": {
+			"index": 1
+		}
+	},
+	"gene_forehead_width": {
+		"index": 14,
+		"forehead_width_neg": {
+			"index": 0
+		},
+		"forehead_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_forehead_height": {
+		"index": 15,
+		"forehead_height_neg": {
+			"index": 0
+		},
+		"forehead_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_head_height": {
+		"index": 16,
+		"head_height_neg": {
+			"index": 0
+		},
+		"head_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_head_width": {
+		"index": 17,
+		"head_width_neg": {
+			"index": 0
+		},
+		"head_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_head_profile": {
+		"index": 18,
+		"head_profile_neg": {
+			"index": 0
+		},
+		"head_profile_pos": {
+			"index": 1
+		}
+	},
+	"gene_head_top_height": {
+		"index": 19,
+		"head_top_height_neg": {
+			"index": 0
+		},
+		"head_top_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_head_top_width": {
+		"index": 20,
+		"head_top_width_neg": {
+			"index": 0
+		},
+		"head_top_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_jaw_angle": {
+		"index": 21,
+		"jaw_angle_neg": {
+			"index": 0
+		},
+		"jaw_angle_pos": {
+			"index": 1
+		}
+	},
+	"gene_jaw_forward": {
+		"index": 22,
+		"jaw_forward_neg": {
+			"index": 0
+		},
+		"jaw_forward_pos": {
+			"index": 1
+		}
+	},
+	"gene_jaw_height": {
+		"index": 23,
+		"jaw_height_neg": {
+			"index": 0
+		},
+		"jaw_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_jaw_width": {
+		"index": 24,
+		"jaw_width_neg": {
+			"index": 0
+		},
+		"jaw_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_mouth_corner_depth": {
+		"index": 25,
+		"mouth_corner_depth_neg": {
+			"index": 0
+		},
+		"mouth_corner_depth_pos": {
+			"index": 1
+		}
+	},
+	"gene_mouth_corner_height": {
+		"index": 26,
+		"mouth_corner_height_neg": {
+			"index": 0
+		},
+		"mouth_corner_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_mouth_forward": {
+		"index": 27,
+		"mouth_forward_neg": {
+			"index": 0
+		},
+		"mouth_forward_pos": {
+			"index": 1
+		}
+	},
+	"gene_mouth_height": {
+		"index": 28,
+		"mouth_height_neg": {
+			"index": 0
+		},
+		"mouth_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_mouth_width": {
+		"index": 29,
+		"mouth_width_neg": {
+			"index": 0
+		},
+		"mouth_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_mouth_upper_lip_size": {
+		"index": 30,
+		"mouth_upper_lip_size_neg": {
+			"index": 0
+		},
+		"mouth_upper_lip_size_pos": {
+			"index": 1
+		}
+	},
+	"gene_mouth_lower_lip_size": {
+		"index": 31,
+		"mouth_lower_lip_size_neg": {
+			"index": 0
+		},
+		"mouth_lower_lip_size_pos": {
+			"index": 1
+		}
+	},
+	"gene_mouth_open": {
+		"index": 32,
+		"mouth_open_neg": {
+			"index": 0
+		},
+		"mouth_open_pos": {
+			"index": 1
+		}
+	},
+	"gene_neck_length": {
+		"index": 33,
+		"neck_length_neg": {
+			"index": 0
+		},
+		"neck_length_pos": {
+			"index": 1
+		}
+	},
+	"gene_neck_width": {
+		"index": 34,
+		"neck_width_neg": {
+			"index": 0
+		},
+		"neck_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_cheek_forward": {
+		"index": 35,
+		"cheek_forward_neg": {
+			"index": 0
+		},
+		"cheek_forward_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_cheek_height": {
+		"index": 36,
+		"cheek_height_neg": {
+			"index": 0
+		},
+		"cheek_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_cheek_width": {
+		"index": 37,
+		"cheek_width_neg": {
+			"index": 0
+		},
+		"cheek_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_ear_angle": {
+		"index": 38,
+		"ear_angle_neg": {
+			"index": 0
+		},
+		"ear_angle_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_ear_inner_shape": {
+		"index": 39,
+		",earinner_shape_pos": {
+			"index": 0
+		}
+	},
+	"gene_bs_ear_bend": {
+		"index": 40,
+		"ear_lower_bend_pos": {
+			"index": 0
+		},
+		"ear_upper_bend_pos": {
+			"index": 1
+		},
+		"ear_both_bend_pos": {
+			"index": 2
+		}
+	},
+	"gene_bs_ear_outward": {
+		"index": 41,
+		"ear_outward_neg": {
+			"index": 0
+		},
+		"ear_outward_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_ear_size": {
+		"index": 42,
+		"ear_size_neg": {
+			"index": 0
+		},
+		"ear_size_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_eye_corner_depth": {
+		"index": 43,
+		"eye_corner_depth_neg": {
+			"index": 0
+		},
+		"eye_corner_depth_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_eye_fold_shape": {
+		"index": 44,
+		"eye_fold_shape_neg": {
+			"index": 0
+		},
+		"eye_fold_shape_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_eye_size": {
+		"index": 45,
+		"eye_size_neg": {
+			"index": 0
+		},
+		"eye_size_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_eye_upper_lid_size": {
+		"index": 46,
+		"eye_upper_lid_size_neg": {
+			"index": 0
+		},
+		"eye_upper_lid_size_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_forehead_brow_curve": {
+		"index": 47,
+		"forehead_brow_curve_neg": {
+			"index": 0
+		},
+		"forehead_brow_curve_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_forehead_brow_forward": {
+		"index": 48,
+		"forehead_brow_forward_neg": {
+			"index": 0
+		},
+		"forehead_brow_forward_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_forehead_brow_inner_height": {
+		"index": 49,
+		"forehead_brow_inner_height_neg": {
+			"index": 0
+		},
+		"forehead_brow_inner_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_forehead_brow_outer_height": {
+		"index": 50,
+		"forehead_brow_outer_height_neg": {
+			"index": 0
+		},
+		"forehead_brow_outer_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_forehead_brow_width": {
+		"index": 51,
+		"forehead_brow_width_neg": {
+			"index": 0
+		},
+		"forehead_brow_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_jaw_def": {
+		"index": 52,
+		"jaw_def_neg": {
+			"index": 0
+		},
+		"jaw_def_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_mouth_lower_lip_def": {
+		"index": 53,
+		"mouth_lower_lip_def_pos": {
+			"index": 0
+		}
+	},
+	"gene_bs_mouth_lower_lip_full": {
+		"index": 54,
+		"mouth_lower_lip_full_neg": {
+			"index": 0
+		},
+		"mouth_lower_lip_full_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_mouth_lower_lip_pad": {
+		"index": 55,
+		"mouth_lower_lip_pad_neg": {
+			"index": 0
+		},
+		"mouth_lower_lip_pad_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_mouth_lower_lip_width": {
+		"index": 56,
+		"mouth_lower_lip_width_neg": {
+			"index": 0
+		},
+		"mouth_lower_lip_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_mouth_philtrum_def": {
+		"index": 57,
+		"mouth_philtrum_def_pos": {
+			"index": 0
+		}
+	},
+	"gene_bs_mouth_philtrum_shape": {
+		"index": 58,
+		"mouth_philtrum_shape_neg": {
+			"index": 0
+		},
+		"mouth_philtrum_shape_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_mouth_philtrum_width": {
+		"index": 59,
+		"mouth_philtrum_width_neg": {
+			"index": 0
+		},
+		"mouth_philtrum_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_mouth_upper_lip_def": {
+		"index": 60,
+		"mouth_upper_lip_def_pos": {
+			"index": 0
+		}
+	},
+	"gene_bs_mouth_upper_lip_full": {
+		"index": 61,
+		"mouth_upper_lip_full_neg": {
+			"index": 0
+		},
+		"mouth_upper_lip_full_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_mouth_upper_lip_profile": {
+		"index": 62,
+		"mouth_upper_lip_profile_neg": {
+			"index": 0
+		},
+		"mouth_upper_lip_profile_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_mouth_upper_lip_width": {
+		"index": 63,
+		"mouth_upper_lip_width_neg": {
+			"index": 0
+		},
+		"mouth_upper_lip_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_nose_forward": {
+		"index": 64,
+		"nose_forward_neg": {
+			"index": 0
+		},
+		"nose_forward_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_nose_height": {
+		"index": 65,
+		"nose_height_neg": {
+			"index": 0
+		},
+		"nose_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_nose_length": {
+		"index": 66,
+		"nose_length_neg": {
+			"index": 0
+		},
+		"nose_length_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_nose_nostril_height": {
+		"index": 67,
+		"nose_nostril_height_neg": {
+			"index": 0
+		},
+		"nose_nostril_height_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_nose_nostril_width": {
+		"index": 68,
+		"nose_nostril_width_neg": {
+			"index": 0
+		},
+		"nose_nostril_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_nose_profile": {
+		"index": 69,
+		"nose_profile_neg": {
+			"index": 0
+		},
+		"nose_profile_pos": {
+			"index": 1
+		},
+		"nose_profile_hawk": {
+			"index": 2
+		},
+		"nose_profile_hawk_pos": {
+			"index": 3
+		}
+	},
+	"gene_bs_nose_ridge_angle": {
+		"index": 70,
+		"nose_ridge_angle_neg": {
+			"index": 0
+		},
+		"nose_ridge_angle_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_nose_ridge_width": {
+		"index": 71,
+		"nose_ridge_width_neg": {
+			"index": 0
+		},
+		"nose_ridge_width_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_nose_size": {
+		"index": 72,
+		"nose_size_neg": {
+			"index": 0
+		},
+		"nose_size_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_nose_tip_angle": {
+		"index": 73,
+		"nose_tip_angle_neg": {
+			"index": 0
+		},
+		"nose_tip_angle_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_nose_tip_forward": {
+		"index": 74,
+		"nose_tip_forward_neg": {
+			"index": 0
+		},
+		"nose_tip_forward_pos": {
+			"index": 1
+		}
+	},
+	"gene_bs_nose_tip_width": {
+		"index": 75,
+		"nose_tip_width_neg": {
+			"index": 0
+		},
+		"nose_tip_width_pos": {
+			"index": 1
+		}
+	},
+	"face_detail_cheek_def": {
+		"index": 76,
+
+		"cheek_def_01": {
+			"index": 0
+		},
+
+		"cheek_def_02": {
+			"index": 1
+		}
+	},
+	"face_detail_cheek_fat": {
+		"index": 77,
+
+		"cheek_fat_01_pos": {
+			"index": 0
+		},
+		"cheek_fat_02_pos": {
+			"index": 1
+		},
+		"cheek_fat_03_pos": {
+			"index": 2
+		},
+		"cheek_fat_04_pos": {
+			"index": 3
+		},
+		"cheek_fat_01_neg": {
+			"index": 4
+		}
+	},
+
+	"face_detail_chin_cleft": {
+		"index": 78,
+		"chin_cleft": {
+			"index": 0
+		},
+		"chin_dimple": {
+			"index": 1
+		}
+	},
+	"face_detail_chin_def": {
+		"index": 79,
+		"chin_def": {
+			"index": 0
+		}
+	}
+
+
+	,
+	"face_detail_eye_lower_lid_def": {
+		"index": 80,
+		"eye_lower_lid_def": {
+			"index": 0
+		}
+	}
+
+	,
+	"face_detail_eye_socket": {
+		"index": 81,
+		"eye_socket_01": {
+			"index": 0
+		},
+		"eye_socket_02": {
+			"index": 1
+		},
+		"eye_socket_03": {
+			"index": 2
+		}
+	},
+
+
+	"face_detail_nasolabial": {
+		"index": 82,
+
+		"nasolabial_01": {
+			"index": 0
+		},
+		"nasolabial_02": {
+			"index": 1
+		},
+		"nasolabial_03": {
+			"index": 2
+		},
+		"nasolabial_04": {
+			"index": 3
+		}
+	},
+
+	"face_detail_nose_ridge_def": {
+		"index": 83,
+		"nose_ridge_def_pos": {
+			"index": 0
+		},
+		"nose_ridge_def_neg": {
+			"index": 1
+		}
+	},
+
+	"face_detail_nose_tip_def": {
+		"index": 84,
+		"nose_tip_def": {
+			"index": 0
+		}
+	},
+	"face_detail_temple_def": {
+		"index": 85,
+
+		"temple_def": {
+			"index": 0
+		}
+	},
+
+
+	"expression_brow_wrinkles": {
+		"index": 86,
+
+		"brow_wrinkles_01": {
+			"index": 0
+		},
+		"brow_wrinkles_02": {
+			"index": 1
+		},
+		"brow_wrinkles_03": {
+			"index": 2
+		},
+		"brow_wrinkles_04": {
+			"index": 3
+		}
+	},
+	"expression_eye_wrinkles": {
+		"index": 87,
+
+		"eye_wrinkles_01": {
+			"index": 0
+		},
+		"eye_wrinkles_02": {
+			"index": 1
+		},
+		"eye_wrinkles_03": {
+			"index": 2
+		}
+	},
+
+	"expression_forehead_wrinkles": {
+		"index": 88,
+
+		"forehead_wrinkles_01": {
+			"index": 0
+		},
+		"forehead_wrinkles_02": {
+			"index": 1
+		},
+		"forehead_wrinkles_03": {
+			"index": 2
+		}
+	},
+
+	"expression_other": {
+		"index": 89,
+
+		"cheek_wrinkles_left_01": {
+			"index": 0
+		},
+		"cheek_wrinkles_right_01": {
+			"index": 1
+		},
+		"cheek_wrinkles_both_01": {
+			"index": 2
+		},
+		"nose_wrinkles_01": {
+			"index": 3
+		}
+	},
+
+
+
+
+
+
+	"complexion": {
+		"index": 90,
+
+		"complexion_1": {
+			"index": 0
+		},
+		"complexion_2": {
+			"index": 1
+		},
+		"complexion_3": {
+			"index": 2
+		},
+		"complexion_4": {
+			"index": 3
+		},
+		"complexion_5": {
+			"index": 4
+		},
+		"complexion_6": {
+			"index": 5
+		},
+		"complexion_7": {
+			"index": 6
+		},
+		"complexion_beauty_1": {
+			"index": 7
+		},
+		"complexion_ugly_1": {
+			"index": 8
+		},
+		"complexion_no_face": {
+			"index": 9
+		}
+	},
+	"gene_height": {
+		"index": 91,
+
+		"full_height": {
+			"index": 0
+		},
+
+		"normal_height": {
+			"index": 1
+		},
+
+		"dwarf_height": {
+			"index": 2
+		},
+
+		"giant_height": {
+			"index": 3
+		}
+	},
+	"gene_bs_body_type": {
+		"index": 92,
+
+		"body_average": {
+			"index": 0
+		},
+		"body_fat_head_fat_low": {
+			"index": 1
+		},
+		"body_fat_head_fat_medium": {
+			"index": 2
+		},
+
+		"body_fat_head_fat_full": {
+			"index": 3
+		},
+
+		"no_portrait": {
+			"index": 4
+		}
+	},
+	"gene_bs_body_shape": {
+		"index": 93,
+
+		"body_shape_average_clothed": {
+			"index": 0
+		},
+		"body_shape_aver": {
+			"index": 1
+		},
+
+		"body_shape_apple_half": {
+			"index": 2
+		},
+
+		"body_shape_apple_full": {
+			"index": 3
+		},
+
+		"body_shape_hourglass_half": {
+			"index": 4
+		},
+
+		"body_shape_hourglass_full": {
+			"index": 5
+		},
+
+		"body_shape_pear_half": {
+			"index": 6
+		},
+
+		"body_shape_pear_full": {
+			"index": 7
+		},
+
+		"body_shape_rectangle_half": {
+			"index": 8
+		},
+
+		"body_shape_rectangle_full": {
+			"index": 9
+		},
+
+		"body_shape_triangle_half": {
+			"index": 10
+		},
+
+		"body_shape_triangle_full": {
+			"index": 11
+		}
+	},
+	"gene_bs_bust": {
+		"index": 94,
+
+		"bust_clothes": {
+			"index": 0
+		},
+
+		"bust_default": {
+			"index": 1
+		},
+
+		"bust_shape_1_half": {
+			"index": 2
+		},
+
+		"bust_shape_1_full": {
+			"index": 3
+		},
+		"bust_shape_2_half": {
+			"index": 4
+		},
+
+		"bust_shape_2_full": {
+			"index": 5
+		},
+
+		"bust_shape_3_half": {
+			"index": 6
+		},
+
+		"bust_shape_3_full": {
+			"index": 7
+		},
+
+		"bust_shape_4_half": {
+			"index": 8
+		},
+
+		"bust_shape_4_full": {
+			"index": 9
+		}
+	},
+	"gene_age": {
+		"index": 95,
+		"old_1": {
+			"index": 0
+		},
+		"old_2": {
+			"index": 1
+		},
+		"old_3": {
+			"index": 2
+		},
+		"old_4": {
+			"index": 3
+		},
+		"old_beauty_1": {
+			"index": 4
+		},
+		"no_aging": {
+			"index": 5
+		}
+	},
+
+	"gene_eyebrows_shape": {
+		"index": 96,
+		"no_eyebrows": {
+			"index": 0
+		},
+		"avg_spacing_avg_thickness": {
+			"index": 1
+		},
+		"avg_spacing_high_thickness": {
+			"index": 2
+		},
+		"avg_spacing_low_thickness": {
+			"index": 3
+		},
+		"avg_spacing_lower_thickness": {
+			"index": 4
+		},
+		"far_spacing_avg_thickness": {
+			"index": 5
+		},
+		"far_spacing_high_thickness": {
+			"index": 6
+		},
+		"far_spacing_low_thickness": {
+			"index": 7
+		},
+		"far_spacing_lower_thickness": {
+			"index": 8
+		},
+		"close_spacing_avg_thickness": {
+			"index": 9
+		},
+		"close_spacing_high_thickness": {
+			"index": 10
+		},
+		"close_spacing_low_thickness": {
+			"index": 11
+		},
+		"close_spacing_lower_thickness": {
+			"index": 12
+		}
+	},
+
+	"gene_eyebrows_fullness": {
+		"index": 97,
+		"no_eyebrows": {
+			"index": 0
+		},
+		"layer_2_avg_thickness": {
+			"index": 1
+		},
+		"layer_2_high_thickness": {
+			"index": 2
+		},
+		"layer_2_low_thickness": {
+			"index": 3
+		},
+		"layer_2_lower_thickness": {
+			"index": 4
+		}
+	},
+	"gene_body_hair": {
+		"index": 98,
+		"body_hair_sparse": {
+			"index": 0
+		},
+		"body_hair_avg": {
+			"index": 1
+		},
+		"body_hair_dense": {
+			"index": 2
+		},
+		"body_hair_sparse_low_stubble": {
+			"index": 3
+		},
+		"body_hair_avg_low_stubble": {
+			"index": 4
+		},
+		"body_hair_dense_low_stubble": {
+			"index": 5
+		}
+	},
+	"hairstyles": {
+		"index": 99,
+		"all_hairstyles": {
+			"index": 0
+		},
+		"no_hairstyles": {
+			"index": 1
+		},
+		"western_hairstyles": {
+			"index": 2
+		},
+		"mena_hairstyles": {
+			"index": 3
+		},
+		"byzantine_hairstyles": {
+			"index": 4
+		},
+		"sub_saharan_hairstyles": {
+			"index": 5
+		},
+		"rtt_hairstyles": {
+			"index": 6
+		},
+		"indian_hairstyles": {
+			"index": 7
+		},
+		"northern_hairstyles": {
+			"index": 8
+		},
+		"steppe_hairstyles": {
+			"index": 9
+		},
+		"catholic_devoted_hairstyles": {
+			"index": 10
+		},
+		"western_baby_hairstyles": {
+			"index": 11
+		},
+		"sub_saharan_baby_hairstyles": {
+			"index": 12
+		}
+	},
+	"beards": {
+		"index": 100,
+
+		"no_beard": {
+			"index": 0
+		},
+
+		"all_beards": {
+			"index": 1
+		},
+
+		"western_beards": {
+			"index": 2
+		},
+
+		"rtt_beards": {
+			"index": 3
+		},
+
+
+		"mena_beards": {
+			"index": 4
+		},
+
+
+		"northern_beards": {
+			"index": 5
+		},
+
+		"steppe_beards": {
+			"index": 6
+		},
+
+		"sub_saharan_beards": {
+			"index": 7
+		},
+
+		"indian_beards": {
+			"index": 8
+		}
+
+	},
+	"eye_accessory": {
+		"index": 101,
+		"normal_eyes": {
+			"index": 0
+		},
+		"normal_eyes_no_shadow": {
+			"index": 1
+		},
+		"normal_eyes_dark_iris": {
+			"index": 2
+		},
+		"normal_eyes_asian": {
+			"index": 3
+		},
+		"bloodshot_eyes": {
+			"index": 4
+		},
+		"blind_eyes": {
+			"index": 5
+		},
+		"no_eyes": {
+			"index": 6
+		}
+	},
+
+	"teeth_accessory": {
+
+		"index": 102,
+		"normal_teeth": {
+			"index": 0
+		},
+		"no_teeth": {
+			"index": 1
+		}
+	},
+
+	"eyelashes_accessory": {
+
+		"index": 103,
+		"no_eyelashes": {
+			"index": 0
+		},
+
+		"normal_eyelashes": {
+			"index": 1
+		},
+
+		"asian_eyelashes": {
+			"index": 2
+		}
+	}
+}`;
